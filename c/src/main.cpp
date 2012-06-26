c327cf463f9bc8651b9030fa7757eec57750dc30
kerschem <kerschem@fc0f73c3-6fad-4dd8-87c9-2cd631c2c9c4>
2012-06-26 08:53:30 +0000

    created testcases
    git-svn-id: https://svn.model.in.tum.de/svn/newton@59 fc0f73c3-6fad-4dd8-87c9-2cd631c2c9c4

--- c/src/main.cpp
+++ c/src/main.cpp
@@ -3,33 +3,26 @@
 #include "matrix.h"
 #include "polynomial.h"
 
-int main(int argc, char* argv[])
+void test_addition()
 {
-	FloatSemiring foo(0.5);
-	FloatSemiring bar(0.2);
+	FloatSemiring first(0.5);
+	FloatSemiring second(0.3);
+	FloatSemiring result = first + second;
+	std::cout << "- float semiring addition: " << std::endl;
+	std::cout << first << " + " << second << " = " << result << std::endl;
+}
 
-	Matrix<FloatSemiring> bazinga(3,3,FloatSemiring(0.6));
-	bazinga = bazinga + bazinga;
+void test_multiplication()
+{
+	FloatSemiring first(0.5);
+	FloatSemiring second(0.3);
+	FloatSemiring result = first * second;
+	std::cout << "- float semiring multiplication: " << std::endl;
+	std::cout << first << " * " << second << " = " << result << std::endl;
+}
 
-/*	FloatSemiring elems[] = {
-		FloatSemiring(0.5), FloatSemiring(0.9),
-		FloatSemiring(0.2), FloatSemiring(0.1)};
-	Matrix<FloatSemiring> foobar(2,2,std::vector<FloatSemiring>(elems, elems+4));
-	foobar = foobar * foobar;
-*/
-
-	FloatSemiring elems[] = {
-		FloatSemiring(0.5), FloatSemiring(0.4), FloatSemiring(0.3),
-		FloatSemiring(0.2), FloatSemiring(0.1), FloatSemiring(0.4),
-		FloatSemiring(0.3), FloatSemiring(0.2), FloatSemiring(0.2)};
-	Matrix<FloatSemiring> foobar2(3,3,std::vector<FloatSemiring>(elems, elems+9));
-
-	std::cout << foobar2 << std::endl;
-	Matrix<FloatSemiring> res = foobar2.star();
-	std::cout << "star of this:" << std::endl;
-	std::cout << res << std::endl;
-
-/*
+Polynomial<FloatSemiring> get_first_polynomial()
+{
 	// define new polynomial 2xx+7y
 	std::map<std::string, FloatSemiring> coeff;
 	coeff["xx"] = FloatSemiring(2);
@@ -37,27 +30,144 @@ int main(int argc, char* argv[])
 	std::set<char> variables;
 	variables.insert('x');
 	variables.insert('z');
-	Polynomial<FloatSemiring> poly1(variables,coeff);
+	return Polynomial<FloatSemiring>(variables,coeff);
+}
 
+Polynomial<FloatSemiring> get_second_polynomial()
+{
 	// define new polynomial 5xx+3xy+6yy
-	coeff.clear();
+	std::map<std::string, FloatSemiring> coeff;
 	coeff["xx"] = FloatSemiring(5);
 	coeff["xy"] = FloatSemiring(3);
 	coeff["yy"] = FloatSemiring(6);
-	variables.clear();
+	std::set<char> variables;
 	variables.insert('x');
 	variables.insert('y');
-	Polynomial<FloatSemiring> poly2(variables,coeff);
+	return Polynomial<FloatSemiring>(variables,coeff);
+}
 
-	Polynomial<FloatSemiring> tmpPoly;
-	tmpPoly = poly1 + poly2;
-	std::cout << "(" << poly1 << ") + (" << poly2 << ") = " << tmpPoly << std::endl;
-	tmpPoly = poly2 * poly2;
-	std::cout << "(" << poly2 << ")^2 = " << tmpPoly << std::endl;
+void test_polynomial_addition()
+{
+	Polynomial<FloatSemiring> first = get_first_polynomial();
+	Polynomial<FloatSemiring> second = get_second_polynomial();
+	Polynomial<FloatSemiring> result = first + second;
+	std::cout << "- polynomial addition:" << std::endl;
+	std::cout << first << " + " << second << " = " << result << std::endl;
+}
 
-	std::cout << "d/dx(" << tmpPoly << ") = ";
-	tmpPoly = tmpPoly.derivative('x');
-	std::cout << tmpPoly << std::endl;
-*/
+void test_polynomial_multiplication()
+{
+	Polynomial<FloatSemiring> first = get_first_polynomial();
+	Polynomial<FloatSemiring> second = get_second_polynomial();
+	Polynomial<FloatSemiring> result = first * second;
+	std::cout << "- polynomial multiplication:" << std::endl;
+	std::cout << first << " * " << second << " = " << result << std::endl;
+}
+
+std::map<char,FloatSemiring> get_variable_values()
+{
+	std::map<char,FloatSemiring> values;
+	values.insert(values.begin(), std::pair<char,FloatSemiring>('x',FloatSemiring(5)));
+	values.insert(values.begin(), std::pair<char,FloatSemiring>('y',FloatSemiring(4)));
+	values.insert(values.begin(), std::pair<char,FloatSemiring>('z',FloatSemiring(0)));
+	return values;
+}
+
+void test_polynomial_evaluation()
+{
+	Polynomial<FloatSemiring> polynomial = get_second_polynomial();
+	std::map<char,FloatSemiring> values = get_variable_values();
+	FloatSemiring result = polynomial.eval(values);
+	std::cout << "- polynomial evaluation:" << std::endl;
+	std::cout << polynomial << " at {x=5, y=4, z=0} = " << result << std::endl;
+}
+
+Matrix<FloatSemiring> get_first_matrix()
+{
+	FloatSemiring elems[] = {
+		FloatSemiring(0.5), FloatSemiring(0.9),
+		FloatSemiring(0.2), FloatSemiring(0.1)};
+	return Matrix<FloatSemiring>(2,2,std::vector<FloatSemiring>(elems, elems+4));
+}
+
+Matrix<FloatSemiring> get_second_matrix()
+{
+	FloatSemiring elems[] = {
+		FloatSemiring(0.3), FloatSemiring(0.1),
+		FloatSemiring(0.7), FloatSemiring(0.2)};
+	return Matrix<FloatSemiring>(2,2,std::vector<FloatSemiring>(elems, elems+4));
+}
+
+void test_matrix_addition()
+{
+	Matrix<FloatSemiring> first = get_first_matrix();
+	Matrix<FloatSemiring> second = get_second_matrix();
+	Matrix<FloatSemiring> result = first + second;
+	std::cout << "- matrix addition:" << std::endl;
+	std::cout << first << " + " << std::endl << second << " = " << std::endl << result;
+}
+
+void test_matrix_multiplication()
+{
+	Matrix<FloatSemiring> first = get_first_matrix();
+	Matrix<FloatSemiring> second = get_second_matrix();
+	Matrix<FloatSemiring> result = first * second;
+	std::cout << "- matrix multiplication:" << std::endl;
+	std::cout << first << " * " << std::endl << second << " = " << std::endl << result;
+}
+
+void test_matrix_star()
+{
+	Matrix<FloatSemiring> matrix = get_first_matrix();
+	Matrix<FloatSemiring> result = matrix.star();
+	std::cout << "- matrix star:" << std::endl;
+	std::cout << matrix << " the matrix star of this is: " << std::endl << result;
+}
+
+void test_jacobian()
+{
+	std::vector<char> variables;
+	variables.push_back('x');
+	variables.push_back('y');
+	variables.push_back('z');
+	std::vector<Polynomial<FloatSemiring> > polynomials;
+	polynomials.push_back(get_first_polynomial());
+	polynomials.push_back(get_second_polynomial());
+	Matrix<Polynomial<FloatSemiring> > jacobian = Polynomial<FloatSemiring>::jacobian(polynomials, variables);
+	std::cout << "- jacobian (x,y,z) of " << std::endl;
+	std::cout << polynomials.at(0) << std::endl << polynomials.at(1) << std::endl << " = " << std::endl << jacobian << std::endl;
+}
+
+void test_polynomial_matrix_evaluation()
+{
+	std::vector<char> variables;
+	variables.push_back('x');
+	variables.push_back('y');
+	variables.push_back('z');
+	std::vector<Polynomial<FloatSemiring> > polynomials;
+	polynomials.push_back(get_first_polynomial());
+	polynomials.push_back(get_second_polynomial());
+	Matrix<Polynomial<FloatSemiring> > polynomial_matrix = Polynomial<FloatSemiring>::jacobian(polynomials, variables);
+	std::map<char,FloatSemiring> values = get_variable_values();
+	Matrix<FloatSemiring> result = Polynomial<FloatSemiring>::eval(polynomial_matrix, values);
+	std::cout << "- polynomial matrix evaluation" << std::endl;
+	std::cout << polynomial_matrix << " at {x=5, y=4, z=0} = " << std::endl << result;
+}
+
+
+int main(int argc, char* argv[])
+{
+	std::cout << "testing..." << std::endl;
+//	test_addition();
+//	test_multiplication();
+//	test_polynomial_addition();
+//	test_polynomial_multiplication();
+//	test_polynomial_evaluation();
+//	test_matrix_addition();
+//	test_matrix_multiplication();
+//	test_matrix_star();
+//	test_jacobian();
+	test_polynomial_matrix_evaluation();
+
 	return 0;
 }
