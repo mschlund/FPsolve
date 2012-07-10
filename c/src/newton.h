19a1a787847a687b79c3aa22208adecd1516ef74
kerschem <kerschem@fc0f73c3-6fad-4dd8-87c9-2cd631c2c9c4>
2012-07-10 08:54:10 +0000

    implemented kartesian produkt of vectors, symbolic delta and non working newton iteration
    git-svn-id: https://svn.model.in.tum.de/svn/newton@71 fc0f73c3-6fad-4dd8-87c9-2cd631c2c9c4

--- c/src/newton.h
+++ c/src/newton.h
@@ -9,19 +9,126 @@ template <typename SR>
 class Newton
 {
 private:
-	Matrix<Polynomial<SR> > compute_symbolic_delta(const Matrix<Polynomial<SR> >& u, const std::vector<Polynomial<SR> >& F)
+	void xp(const std::vector<std::vector<int>*>& vecs,
+			std::vector<std::vector<int>*> *result) {
+		std::vector<std::vector<int>*>* rslts;
+		for (int ii = 0; ii < vecs.size(); ++ii) {
+			const std::vector<int>& vec = *vecs[ii];
+			if (ii == 0) {
+				// vecs=[[1,2],...] ==> rslts=[[1],[2]]
+				rslts = new std::vector<std::vector<int>*>;
+				for (int jj = 0; jj < vec.size(); ++jj) {
+					std::vector<int>* v = new std::vector<int>;
+					v->push_back(vec[jj]);
+					rslts->push_back(v);
+				}
+			} else {
+				// vecs=[[1,2],[3,4],...] ==> rslts=[[1,3],[1,4],[2,3],[2,4]]
+				std::vector<std::vector<int>*>* tmp = new std::vector<
+						std::vector<int>*>;
+				for (int jj = 0; jj < vec.size(); ++jj) { // vec[jj]=3 (first iter jj=0)
+					for (std::vector<std::vector<int>*>::const_iterator it =
+							rslts->begin(); it != rslts->end(); ++it) {
+						std::vector<int>* v = new std::vector<int>(**it); // v=[1]
+						v->push_back(vec[jj]);                        // v=[1,3]
+						tmp->push_back(v);                        // tmp=[[1,3]]
+					}
+				}
+				for (int kk = 0; kk < rslts->size(); ++kk) {
+					delete (*rslts)[kk];
+				}
+				delete rslts;
+				rslts = tmp;
+			}
+		}
+		result->insert(result->end(), rslts->begin(), rslts->end());
+		delete rslts;
+	}
+
+	Matrix<Polynomial<SR> > compute_symbolic_delta(const std::vector<Polynomial<SR> >& v,
+							const std::vector<Polynomial<SR> >& v_upd,
+							const std::vector<Polynomial<SR> >& F,
+							const std::vector<char>& poly_vars)
 	{
-		std::vector<Polynomial<SR> > ret;
-		for(int i = 0; i<F.size(); i++)
+		std::vector<Polynomial<SR> > delta;
+		int n = v.size();
+
+		for(int i=0; i<n; ++i)
 		{
-			Matrix<Polynomial<SR> > H = F.at(i).hessian();
-			// FIXME: 0.5 cannot be represented universally
-			Matrix<Polynomial<SR> > d = u.transpose() * H * u;
-			ret.push_back(d.getElements().at(0)); // TODO: beautify this
+			Polynomial<SR> delta_i = Polynomial<SR>::null();
+			Polynomial<SR> f = F.at(i);
+			int deg = f.get_degree();
+
+			std::vector<int> list; // holds [0,1,...,deg]
+			for(int j=0; j<deg+1; ++j)
+			{
+				list.push_back(j);
+			}
+			std::vector<std::vector<int>* > tmplist; // holds n-times $list
+			for(int j=0; j<n; ++j)
+			{
+				tmplist.push_back(&list);
+			}
+			std::vector<std::vector<int>* > p; // n-ary cartesian product
+			xp(tmplist,&p); // generate n-ary cartesian product
+
+			//iterate over (x,y,...,z) with x,y,...,z \in [0,deg+1]
+			for(std::vector<std::vector<int>* >::const_iterator it = p.begin(); it != p.end(); ++it)
+			{
+				std::string dx;
+				Polynomial<SR> prod = Polynomial<SR>::one();
+
+				std::vector<char>::const_iterator var = poly_vars.begin();
+				typename std::vector<Polynomial<SR> >::const_iterator elem = v_upd.begin();
+				for(std::vector<int>::const_iterator z = (*it)->begin(); z != (*it)->end(); ++z)
+				{
+					for(int j=0; j<(*z); ++j)
+					{
+						dx += (*var); // generate a string of variables like "xxxyyzzzz";
+						prod = prod * (*elem);	// prod = prod * elem^z ...
+					}
+					++var; // next variable
+					++elem; // next element of vector v_upd
+				}
+
+				// eval f.derivative(dx) at v
+				std::map<char,Polynomial<SR> > values;
+				for(int i = 0; i<poly_vars.size(); i++)
+				{
+					values.insert(values.begin(), std::pair<char,Polynomial<SR> >(poly_vars.at(i),v.at(i)));
+				}
+				Polynomial<SR> f_eval = f.derivative(dx).eval(values);
+
+				delta_i = delta_i + f_eval * prod;
+				//std::cout << delta_i << std::endl;
+			}
+
+			delta.push_back(delta_i);
 		}
 
-		return Matrix<Polynomial<SR> >(1, F.size(), ret);
+		return Matrix<Polynomial<SR> >(1, delta.size(), delta);
 	}
+
+	std::vector<Polynomial<SR> > get_symbolic_vector(const std::vector<char>& poly_vars, int offset)
+	{
+		// define new symbolic vector [u1,u2,...,un] TODO: this is ugly...
+		std::vector<Polynomial<SR> > ret;
+		std::map<std::string, FloatSemiring> coeff;
+		std::set<char> variables;
+		char var[] = "0"; // initialize char*
+		for(int i=0; i<poly_vars.size(); i++)
+		{
+			coeff.clear();
+			variables.clear();
+			sprintf(var,"%d",i+offset);
+			coeff[var] = SR::one(); // variable "1", "2",...
+			variables.insert(var[0]);
+			Polynomial<SR> f = Polynomial<SR>(variables, coeff);
+			ret.push_back(f);
+		}
+		return ret;
+	}
+
 public:
 	// calculate the next newton iterand
 	Matrix<Polynomial<SR> > step(const std::vector<char>& poly_vars, const Matrix<Polynomial<SR> >& J_s,
@@ -41,31 +148,48 @@ public:
 	// iterate until convergence
 	Matrix<Polynomial<SR> > solve_fixpoint(const std::vector<Polynomial<SR> >& F, const std::vector<char>& poly_vars, int max_iter)
 	{
+		Matrix<Polynomial<SR> > F_mat = Matrix<Polynomial<SR> >(1,F.size(),F);
 		Matrix<Polynomial<SR> > J = Polynomial<SR>::jacobian(F, poly_vars);
 		Matrix<Polynomial<SR> > J_s = J.star();
-		// define new symbolic vector [u1,u2,...,un] TODO: this is ugly...
-		std::vector<Polynomial<SR> > ret;
-		std::map<std::string, FloatSemiring> coeff;
-		std::set<char> variables;
-		char var[] = "0"; // initialize char*
-		for(int i=0; i<poly_vars.size(); i++)
-		{
-			coeff.clear();
-			variables.clear();
-			sprintf(var,"%d",i);
-			coeff[var] = SR::one(); // variable "1", "2",...
-			variables.insert(var[0]);
-			Polynomial<SR> f = Polynomial<SR>(variables, coeff);
-			ret.push_back(f);
-		}
-		Matrix<Polynomial<SR> > u = Matrix<Polynomial<SR> >(1, F.size(), ret);
-		Matrix<Polynomial<SR> > delta = compute_symbolic_delta(u, F);
+
+		std::cout << J_s;
+
+		// define new symbolic vectors [u1,u2,...,un] TODO: this is ugly...
+		std::vector<Polynomial<SR> > u = this->get_symbolic_vector(poly_vars, 0);
+		std::vector<Polynomial<SR> > u_upd = this->get_symbolic_vector(poly_vars, 5);
+
+		//Matrix<Polynomial<SR> > u = Matrix<Polynomial<SR> >(1, F.size(), ret);
+		Matrix<Polynomial<SR> > delta = compute_symbolic_delta(u, u_upd, F, poly_vars);
+
+
 
 		Matrix<Polynomial<SR> > v = Matrix<Polynomial<SR> >(1,(int)F.size()); // v^0 = 0
 
+		// d^0 = F(0)
+		std::map<char,Polynomial<SR> > values;
+		Polynomial<SR> null = Polynomial<SR>::null();
+		for(int i = 0; i<poly_vars.size(); i++)
+		{
+			values.insert(values.begin(), std::pair<char,Polynomial<SR> >(poly_vars.at(i), null));
+		}
+		Matrix<Polynomial<SR> > delta_new = Polynomial<SR>::eval(F_mat, values);
+
 		Matrix<Polynomial<SR> > v_upd = step(poly_vars, J_s, v, delta);
 
-		std::cout << v_upd;
+		for(int i=0; i<max_iter; ++i)
+		{
+			/*
+			for(int i = 0; i<u.size(); i++)
+			{
+				values.insert(values.begin(), std::pair<char,Polynomial<SR> >(u_upd.at(i), v_upd.getElements().at(i)));
+				values.insert(values.begin(), std::pair<char,Polynomial<SR> >(u.at(i), v.getElements().at(i)));
+			}
+			delta_new = Polynomial<SR>::eval(delta,values);
+			*/
+			v = v + v_upd;
+			v_upd = step(poly_vars, J_s, v, delta_new);
+		}
+
 		return v_upd;
 	}
 };
