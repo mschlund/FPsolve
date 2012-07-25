971cc8a886a389b5789cf2fd3ad161f44cb09f67
kerschem <kerschem@fc0f73c3-6fad-4dd8-87c9-2cd631c2c9c4>
2012-07-25 14:08:21 +0000

    extract code from header var.h and put it in var.cpp
    git-svn-id: https://svn.model.in.tum.de/svn/newton@91 fc0f73c3-6fad-4dd8-87c9-2cd631c2c9c4

--- c/src/var.h
+++ c/src/var.h
@@ -4,6 +4,8 @@
 #include <string>
 #include <map>
 #include <sstream>
+#include <set>
+#include <vector>
 
 class Var
 {
@@ -13,94 +15,17 @@ private:
 	static int max_id;
 	static std::map<std::string, int> vars; // name → id
 public:
-	Var()
-	{
-		this->id = max_id;
-		std::stringstream ss;
-		ss << max_id;
-		this->name = ss.str();
-		Var::max_id++;
-		Var::vars.insert(Var::vars.begin(), std::pair<std::string,int>(this->name, this->id));
-	}
-
+	Var();
 	// if there is already a variable with this name, return a reference to this variable
-	Var(std::string name)
-	{
-		std::map<std::string, int>::const_iterator v = Var::vars.find(name);
-		this->name = name;
-		if(v != Var::vars.end()) // variable already exists
-		{
-			this->id = v->second;
-		}
-		else
-		{
-			this->id = max_id;
-			Var::max_id++;
-			Var::vars.insert(Var::vars.begin(), std::pair<std::string,int>(this->name, this->id));
-		}
-	}
-
-	bool operator==(const Var& var) const
-	{
-		return this->id == var.id;
-	}
-
-	bool operator!=(const Var& var) const
-	{
-		return this->id != var.id;
-	}
-
-	bool operator<(const Var& var) const
-	{
-		return this->id<var.id;
-	}
-
-	std::string string() const
-	{
-		std::stringstream ss;
-		ss << "Var(" << this->name << ")";
-		return ss.str();
-	}
+	Var(std::string name);
+	bool operator==(const Var& var) const;
+	bool operator!=(const Var& var) const;
+	bool operator<(const Var& var) const;
+	std::string string() const;
 };
 
-int Var::max_id = 0;
-std::map<std::string, int> Var::vars = {};
-
-std::ostream& operator<<(std::ostream& os, const Var var)
-{
-	return os << var.string();
-}
-
-std::ostream& operator<<(std::ostream& os, const std::multiset<Var> vars)
-{
-	std::stringstream ss;
-	ss << "{";
-	for(std::multiset<Var>::const_iterator var = vars.begin(); var != vars.end(); ++var)
-	{
-		if(var != vars.begin())
-		{
-			ss << ",";
-		}
-		ss << (*var).string();
-	}
-	ss << "}";
-	return os << ss.str();
-}
-
-std::ostream& operator<<(std::ostream& os, const std::vector<Var> vars)
-{
-	std::stringstream ss;
-	ss << "{";
-	for(std::vector<Var>::const_iterator var = vars.begin(); var != vars.end(); ++var)
-	{
-		if(var != vars.begin())
-		{
-			ss << ",";
-		}
-		ss << (*var).string();
-	}
-	ss << "}";
-	return os << ss.str();
-}
+std::ostream& operator<<(std::ostream& os, const Var var);
+std::ostream& operator<<(std::ostream& os, const std::multiset<Var> vars);
+std::ostream& operator<<(std::ostream& os, const std::vector<Var> vars);
 
 #endif
