1a4db60cea611fa8a857da1c324dba67fa689f6b
mschlund <schlund@model.in.tum.de>
2013-02-11 11:59:59 +0100

    added parameters for the gen-script

--- gen_eqs.py
+++ gen_eqs.py
@@ -1,12 +1,18 @@
 import numpy.random
 import itertools
 import random
+import sys
 
-#TODO: input arguments for the script..
+# run with: python gen_eqs.py [#vars] [sparseness-parameter in (0,1] ], e.g. "python gen_eqs.py 14 0.7"
+
+N = int(sys.argv[1])
+tol = float(sys.argv[2])
+
 
 # generate a (sparse) probabilistic system of n quadratic equations in n variables with real coefficients in (0,1)
 # return the system (as a vector of symbolic expressions) as well as its variables
-# eps is the "density", i.e. eps*(binom(n,2)) of the coefficients will be non-zero
+# eps is the "density", i.e. eps*(binom(n,2)) of the coefficients will be non-zero.
+# However, at least one coefficient will be non-zero (so the minimum number of terms on each rhs is 2)!
 
 def gen_random_quadratic_eqns(n, eps) :
     poly_vars = ['x%d' %i for i in range(n)]
@@ -15,6 +21,9 @@ def gen_random_quadratic_eqns(n, eps) :
     monomials = [m for m in itertools.combinations(poly_vars,2)]
 
     k = int(eps*(n*n/2 - n/2))
+
+    if(k<2) :
+        k=2
 
     #for the non-zero monomials choose random coefficients from (0,1) that add up to 1
     # this can be done by sampling from a (/the) k-dimensional (symmetric) Dirichlet-distribution
@@ -28,7 +37,9 @@ def gen_random_quadratic_eqns(n, eps) :
         f = f + " | " + str(const_coeff)
         print "<" + poly_vars[i] + ">" + " ::= " + f + ";"
 
-gen_random_quadratic_eqns(14, 0.7)
+
+
+gen_random_quadratic_eqns(N, tol)
 
 
 #TODO: make coefficients symbolic constants -> obtain generator for sl-sets or regexp-SR
