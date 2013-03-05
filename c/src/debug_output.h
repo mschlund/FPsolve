2c740ebcbfbd770f6f145a2ff10b674e0be6d4a6
Michal Terepeta <michal.terepeta@gmail.com>
2013-03-05 15:26:27 +0100

    debug_output: better output

--- c/src/debug_output.h
+++ c/src/debug_output.h
@@ -2,18 +2,19 @@
 
 #include <iostream>
 
+#include <boost/filesystem.hpp>
+
 /*
  * Debugging output -- should be used for any information that helps in
  * debugging (i.e., it's ok if it slows things down).
- *
- * Note that __FUNCTION__ is not part of the standard, but it seems to work fine
- * with GCC and Clang.
  */
 
+#define FILE boost::filesystem::path(__FILE__).filename().c_str()
+
 #ifdef DEBUG_OUTPUT
-#define DEBUG_LOCATION "(" << __FUNCTION__ << ":" << __LINE__ << "):"
-#define DMSG(msg) std::cerr << DEBUG_LOCATION << " " << msg << std::endl;
-#define DOUT(x) std::cerr << DEBUG_LOCATION << " " << x;
+#define DEBUG_LOCATION "(" << FILE << ":" << __LINE__ << ")"
+#define DMSG(msg) std::cerr << DEBUG_LOCATION << "  " << msg << std::endl;
+#define DOUT(x) std::cerr << DEBUG_LOCATION << "  " << x;
 #else
 #define DMSG(msg) (void)0
 #define DOUT(x) (void)0
