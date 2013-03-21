cce2827b5559254571918775885a28738ee8f619
Michal Terepeta <michal.terepeta@gmail.com>
2013-03-21 10:48:43 +0100

    semiring: whitespace only

--- c/src/semirings/semiring.h
+++ c/src/semirings/semiring.h
@@ -10,71 +10,68 @@ enum class Idempotence {NonIdempotent, Idempotent};
 
 template <typename SR, Commutativity Comm, Idempotence Idem>
 class Semiring {
-public:
-  virtual ~Semiring(){};
+  public:
+    virtual ~Semiring(){};
 
-	friend SR operator * (const SR& lhs, const SR& rhs)
-	{
-		SR result = lhs;
-		result *= rhs;
-		return result;
-	}
-  friend SR operator * (const SR& lhs, const std::uint_fast16_t& rhs)
-  {
-    SR result = lhs;
-    result *= rhs;
-    return result;
-  }
-
-	friend SR operator + (const SR& lhs, const SR& rhs)
-	{
-		SR result = lhs;
-		result += rhs;
-		return result;
-	}
-
-	virtual SR star () const = 0;
-	virtual bool operator ==(const SR& elem) const = 0;
-
-	static bool IsCommutative() {
-	  if(Commutativity::Commutative == Comm)
-	    return true;
-	  else
-	    return false;
-	}
-
-  static bool IsIdempotent() {
-    if(Idempotence::Idempotent == Idem)
-      return true;
-    else
-      return false;
-  }
-
-  // for all SRs we have 0*S = 0 and
-  // if the SR is idempotent then for any n in Nat if n>0: n*S = S
-  friend SR operator *= (SR& lhs, const std::uint_fast16_t& cnt) {
-    if(0 == cnt)
-      lhs = SR::null();
-
-    if(Idempotence::NonIdempotent == Idem) {
-      for (std::uint_fast16_t i = 1; i < cnt; ++i) {
-        lhs += lhs;
-      }
+    friend SR operator*(const SR& lhs, const SR& rhs) {
+      SR result = lhs;
+      result *= rhs;
+      return result;
     }
-    return lhs;
-  }
 
-	static SR null();
-	static SR one();
-	virtual std::string string() const = 0;
+    friend SR operator*(const SR& lhs, const std::uint_fast16_t& rhs) {
+      SR result = lhs;
+      result *= rhs;
+      return result;
+    }
 
-        // FIXME: This might be _really_ inefficient.  Maybe we should just
-        // require that the semirings provide the specialization for
-        // std::hash...?
-	size_t operator()(const SR& sr) const
-	{
-		return std::hash<std::string>()(sr.string());
-	}
+    friend SR operator+(const SR& lhs, const SR& rhs) {
+      SR result = lhs;
+      result += rhs;
+      return result;
+    }
+
+    virtual SR star () const = 0;
+    virtual bool operator==(const SR& elem) const = 0;
+
+    static bool IsCommutative() {
+      if(Commutativity::Commutative == Comm)
+        return true;
+      else
+        return false;
+    }
+
+    static bool IsIdempotent() {
+      if(Idempotence::Idempotent == Idem)
+        return true;
+      else
+        return false;
+    }
+
+    // for all SRs we have 0*S = 0 and
+    // if the SR is idempotent then for any n in Nat if n>0: n*S = S
+    friend SR operator *= (SR& lhs, const std::uint_fast16_t& cnt) {
+      if(0 == cnt)
+        lhs = SR::null();
+
+      if(Idempotence::NonIdempotent == Idem) {
+        for (std::uint_fast16_t i = 1; i < cnt; ++i) {
+          lhs += lhs;
+        }
+      }
+      return lhs;
+    }
+
+    static SR null();
+    static SR one();
+    virtual std::string string() const = 0;
+
+    // FIXME: This might be _really_ inefficient.  Maybe we should just
+    // require that the semirings provide the specialization for
+    // std::hash...?
+    size_t operator()(const SR& sr) const {
+      return std::hash<std::string>()(sr.string());
+    }
 };
 
 template <typename SR>
@@ -85,9 +82,8 @@ template <typename SR>
 SR operator += (SR& lhs, const SR& rhs);
 
 template <typename SR, Commutativity Comm, Idempotence Idem>
-std::ostream& operator<<(std::ostream& os, const Semiring<SR, Comm, Idem>& elem)
-{
-	return os << elem.string();
+std::ostream& operator<<(std::ostream& os, const Semiring<SR, Comm, Idem>& elem) {
+  return os << elem.string();
 }
 
 
