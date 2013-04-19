47fa6f9578d3a843319db451e78fa5b5edcf405e
Michal Terepeta <michal.terepeta@gmail.com>
2013-04-19 12:03:17 +0200

    float-semiring: move definitions to the header

--- c/src/semirings/float-semiring.h
+++ c/src/semirings/float-semiring.h
@@ -1,8 +1,11 @@
 #ifndef FLOAT_SEMIRING_H
 #define FLOAT_SEMIRING_H
 
+#include <cassert>
+#include <cmath>
+#include <limits>
+#include <sstream>
 #include <string>
-#include <memory>
 
 #include "semiring.h"
 
@@ -10,23 +13,52 @@ class FloatSemiring : public Semiring<FloatSemiring,
                                       Commutativity::Commutative,
                                       Idempotence::NonIdempotent> {
   private:
-    double val;
-    static std::shared_ptr<FloatSemiring> elem_null;
-    static std::shared_ptr<FloatSemiring> elem_one;
+    double value_;
 
   public:
-    FloatSemiring();
-    FloatSemiring(const double val);
-    virtual ~FloatSemiring();
-    FloatSemiring operator += (const FloatSemiring& elem);
-    FloatSemiring operator *= (const FloatSemiring& elem);
-    bool operator == (const FloatSemiring& elem) const;
-    FloatSemiring star () const;
-    static FloatSemiring null();
-    static FloatSemiring one();
-    std::string string() const;
-    static bool is_idempotent;
-    static bool is_commutative;
+    FloatSemiring() : value_(0) {}
+
+    FloatSemiring(const double v) : value_(v) { assert(value_ >= 0); }
+
+    ~FloatSemiring() = default;
+
+    FloatSemiring& operator+=(const FloatSemiring &elem) {
+      value_ += elem.value_;
+      return *this;
+    }
+
+    FloatSemiring& operator*=(const FloatSemiring &elem) {
+      value_ *= elem.value_;
+      return *this;
+    }
+
+    bool operator==(const FloatSemiring &elem) const {
+      // comparing floating point has to be done like this. (see Knuth TAoCP Vol.2 p. 233)
+      return std::fabs(value_ - elem.value_) <=
+             std::numeric_limits<double>::epsilon() *
+               std::min(std::fabs(value_), std::fabs(elem.value_));
+    }
+
+    FloatSemiring star() const {
+      // if value_ == 1 this returns inf
+      assert(0 < 1 - value_);
+      return FloatSemiring(1 / (1 - value_));
+    }
+
+    static FloatSemiring null() {
+      return FloatSemiring{0.0};
+    }
+
+    static FloatSemiring one() {
+      return FloatSemiring{1.0};
+    }
+
+    std::string string() const {
+      std::stringstream ss;
+      ss << value_;
+      return ss.str();
+    }
+
 };
 
 #endif
