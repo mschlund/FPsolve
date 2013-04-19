10b29f0c1ebf63a7cf810f74ddef43436e6f2bf1
Michal Terepeta <michal.terepeta@gmail.com>
2013-04-19 10:26:32 +0200

    float-semiring: whitespace only

--- c/src/semirings/float-semiring.h
+++ c/src/semirings/float-semiring.h
@@ -6,25 +6,27 @@
 
 #include "semiring.h"
 
-class FloatSemiring : public Semiring<FloatSemiring, Commutativity::Commutative, Idempotence::NonIdempotent>
-{
-private:
-	double val;
-	static std::shared_ptr<FloatSemiring> elem_null;
-	static std::shared_ptr<FloatSemiring> elem_one;
-public:
-	FloatSemiring();
-	FloatSemiring(const double val);
-	virtual ~FloatSemiring();
-	FloatSemiring operator += (const FloatSemiring& elem);
-	FloatSemiring operator *= (const FloatSemiring& elem);
-	bool operator == (const FloatSemiring& elem) const;
-	FloatSemiring star () const;
-	static FloatSemiring null();
-	static FloatSemiring one();
-	std::string string() const;
-	static bool is_idempotent;
-	static bool is_commutative;
+class FloatSemiring : public Semiring<FloatSemiring,
+                                      Commutativity::Commutative,
+                                      Idempotence::NonIdempotent> {
+  private:
+    double val;
+    static std::shared_ptr<FloatSemiring> elem_null;
+    static std::shared_ptr<FloatSemiring> elem_one;
+
+  public:
+    FloatSemiring();
+    FloatSemiring(const double val);
+    virtual ~FloatSemiring();
+    FloatSemiring operator += (const FloatSemiring& elem);
+    FloatSemiring operator *= (const FloatSemiring& elem);
+    bool operator == (const FloatSemiring& elem) const;
+    FloatSemiring star () const;
+    static FloatSemiring null();
+    static FloatSemiring one();
+    std::string string() const;
+    static bool is_idempotent;
+    static bool is_commutative;
 };
 
 #endif
