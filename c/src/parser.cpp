2f6aa725b8da9be152b8abf94dd90d1554c47376
mschlund <schlund@model.in.tum.de>
2015-05-26 12:39:23 +0200

    simplified parser (removed EBNF parsing -- was not used really and can be done in preprocessing)

--- c/src/parser.cpp
+++ c/src/parser.cpp
@@ -16,32 +16,6 @@ namespace qi = boost::spirit::qi;
 namespace ascii = boost::spirit::ascii;
 namespace phx = boost::phoenix;
 
-// this function object is used for star'ing from within the parser
-// a new equation with the given new variable is created and returned
-struct star_impl
-{
-	template <typename T, typename SR> struct result { typedef std::pair<VarId, SR> type; }; // needed because this is how boost knows of what type the result will be
-
-	template <typename SR>
-	std::pair<VarId, SR> operator()(VarId var, SR& s) const // var → 1 + var×s
-	{
-		return std::pair<VarId, SR>(var, (SR::one() + SR(var)*s));
-	}
-};
-const phx::function<star_impl> star_equation;
-
-struct option_impl
-{
-	template <typename T> struct result { typedef T type; }; // needed because this is how boost knows of what type the result will be
-
-	template <typename SR>
-	SR operator()(SR& s) const // A → 1 + A
-	{
-		return (SR::one() + s);
-	}
-};
-const phx::function<option_impl> option;
-
 struct rexp_var_impl
 {
         template <typename T>
@@ -109,18 +83,6 @@ struct var_impl
 };
 const phx::function<var_impl> variable;
 
-struct new_var_impl
-{
-	template <typename T>
-	struct result { typedef VarId type; }; // this tells Boost the return type
-
-	template <typename T>
-	const VarId operator()(T t) const // TODO: why has there to be an argument??? compiling fails without dummy argument... overloading???
-	{
-		return Var::GetVarId(); // return a fresh anonymous variable
-	}
-};
-const phx::function<new_var_impl> new_var;
 
 // some boost::qi methods for pushing values through our parser
 using qi::_val;
@@ -195,31 +157,23 @@ struct prefix_elem_parser : qi::grammar<iterator_type, PrefixSemiring()>
         qi::rule<iterator_type, VarId()> var;
 };
 
-
 // use the given parser and return a list of equations of SR polynomials
 // the second parameter is either Polynomial<SR> or NonCommutativePolynomial<SR>
 template <typename SR_Parser, typename PSR>
 struct equation_parser : qi::grammar<iterator_type, std::vector<std::pair<VarId, PSR>>(), qi::space_type>
 {
-	std::vector<std::pair<VarId, PSR>> new_rules; // accumulate generated rules in this vector
 	equation_parser() : equation_parser::base_type(equations)
 	{
-		// the first rule combines the generated equations with the read equations and returns them in one vector
-		equations = (*equation)[_val = _1] [insert(_val, end(_val), begin(phx::ref(new_rules)), end(phx::ref(new_rules)))];
+		equations %= *(equation);
 		equation %= (var >> lexeme["::="] >> polynomial >> ';');
 		polynomial = summand [_val = _1] >> *('|' >> summand [_val = _val + _1]); // addition
 		summand = eps [_val = PSR::one()] >> // set _val to one-element of the semiring
 				*(
 					('(' >> polynomial >> ')') [_val = _val * _1] | // group term
-					('{' >> kstar >> '}') [_val = _val * _1] | // | // kleene star: {A} → B && B = 1 + BA
-					('[' >> polynomial >>']')  [_val = _val * option(_1)] | // optional term: [A] → 1 + A
 					var [_val = _val * _1] | // variable factor
 					sr_elem [_val = _val * _1] // sr-elem factor
 				 );
 		var = varidentifier[_val = variable(_1)];
-		kstar = polynomial	[_a = new_var(0)] // create a new variable _a for the new equation
-					[phx::push_back(phx::ref(new_rules),star_equation(_a,_1))] // create a new equation _a = <stared equation> and save it
-					[_val = _a]; // continue with the new variable
 		varidentifier = qi::as_string[lexeme['<' >> +(ascii::char_ - '>') >> '>']];
 	}
 	qi::rule<iterator_type, std::vector<std::pair<VarId, PSR>>(), qi::space_type> equations;
@@ -227,7 +181,6 @@ struct equation_parser : qi::grammar<iterator_type, std::vector<std::pair<VarId,
 	qi::rule<iterator_type, PSR(), qi::space_type> polynomial;
 	qi::rule<iterator_type, PSR(), qi::space_type> summand;
 	qi::rule<iterator_type, std::string()> varidentifier;
-	qi::rule<iterator_type, VarId(), qi::locals<VarId>, qi::space_type> kstar;
 	qi::rule<iterator_type, VarId()> var;
 	SR_Parser sr_elem;
 };
